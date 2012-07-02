c50e464d1fbe9d444517fb822fb7a8d71f6ff7aa
Tondo <tatsuya.ahad@gmail.com>
2012-07-02 12:38:46 -0400

    Block Pushing Implemented

--- Field.py
+++ Field.py
@@ -31,6 +31,79 @@ class Field(object):
         else:
             print "rotation function given invalid direction"
 
+    def pushBlock(self, plane, loc):
+        if plane == "z" or plane == "-z":
+            if loc >= self.width * self.height:
+                print "Invalid location index"
+                return
+            x = loc % self.width
+            y = loc / self.width
+            zVec = 1 if (plane == "z") else -1
+            self._pushZPlaneBlock(zVec, x, y)
+        elif plane == "x" or plane == "-x":
+            if loc >= self.depth * self.height:
+                print "Invalid location index"
+                return
+            z = loc % self.depth
+            y = loc / self.depth
+            xVec = 1 if (plane == "x") else -1
+            self._pushXPlaneBlock(xVec, y, z)
+        elif plane == "y" or plane == "-y":
+            if loc >= self.depth * self.width:
+                print "Invalid location index"
+                return
+            x = loc % self.width
+            z = loc / self.width
+            yVec = 1 if (plane == "y") else -1
+            self._pushYPlaneBlock(yVec, x, z)
+        else:
+            print "Invalid plane location"
+
+    def _pushXPlaneBlock(self, xVec, y, z):
+        x = (self.width - 1) if (xVec == 1) else 0
+
+        # no block to push
+        if self.field[z][y][x] == 0:
+            return
+
+        x = (self.width - 2) if (xVec == -1) else 1
+
+        while (x >= 0 and x < self.width):
+            if self.field[z][y][x] != 0 and self.field[z][y][x-xVec] == 0:
+                self.field[z][y][x-xVec] = self.field[z][y][x]
+                self.field[z][y][x] = 0
+            x += xVec
+
+    def _pushYPlaneBlock(self, yVec, x, z):
+        y = (self.height - 1) if (yVec == 1) else 0
+
+        # no block to push
+        if self.field[z][y][x] == 0:
+            return
+
+        y = (self.height - 2) if (yVec == -1) else 1
+        
+        while (y >= 0 and y < self.height):
+            if self.field[z][y][x] != 0 and self.field[z][y-yVec][x] == 0:
+                self.field[z][y-yVec][x] = self.field[z][y][x]
+                self.field[z][y][x] = 0
+            y += yVec
+
+    def _pushZPlaneBlock(self, zVec, x, y):
+        z = (self.depth - 1) if (zVec == 1) else 0
+
+        # no block to push
+        if self.field[z][y][x] == 0:
+            return
+
+        z = (self.depth - 2) if (zVec == -1) else 1
+        
+        while (z >= 0 and z < self.depth):
+            if self.field[z][y][x] != 0 and self.field[z-zVec][y][x] == 0:
+                self.field[z-zVec][y][x] = self.field[z][y][x]
+                self.field[z][y][x] = 0
+            z += zVec
+
     def loadFile(self, fileName):
         f = open(fileName, 'r')
         s = f.readline().strip()
@@ -56,9 +129,12 @@ class Field(object):
             for y in range(0, self.height):
                 print self.field[z][y]
             print "-" * (self.width * 5)
+        print
 
 
 if __name__ == "__main__":
     field = Field(4, 3, 2)
     field.loadFile("fieldInit.txt")
+    field.formatPrint()
+    field.pushBlock("y", 3)
     field.formatPrint()
